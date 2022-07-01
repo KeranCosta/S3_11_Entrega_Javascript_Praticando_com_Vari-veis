3fb8233c665b90a4cedcaadfd5108986c8a62848
wilkeran <wilkeran.costs@gmail.com>
2022-07-01 15:33:05 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome= "wilkeran";
+let sobrenome = "costa";
+let anoNascimento = 1993;
+let cidade="maceió";
+
+let idade = 2022-anoNascimento;
+
+console.log(`Eu sou ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`);
